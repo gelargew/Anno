260bf5faabba45b991625fe7e37f3f34ab1cb112
Gelar <gelargew@gmail.com>
2022-12-06 08:29:24 +0700

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { Annotorious } from "@recogito/annotorious";
+import "@recogito/annotorious/dist/annotorious.min.css";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+const img = ref(null);
+const anno = null;
+const innitAnno = () => {};
+onMounted(() => {
+  const anno = new Annotorious({
+    image: img.value,
+  });
+});
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <img ref="img" src="https://w.wallha.com/ws/14/0IWudqMN.jpg" />
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
